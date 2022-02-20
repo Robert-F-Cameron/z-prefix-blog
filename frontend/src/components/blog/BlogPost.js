08314bd05ab1a7e02510e701bc006fee5d79695c
Robert-F-Cameron <robert.f.cameron@gmail.com>
2022-02-20 14:50:03 -0800

    new build

--- frontend/src/components/blog/BlogPost.js
+++ frontend/src/components/blog/BlogPost.js
@@ -55,7 +55,7 @@ export default function BlogPost(props) {
         <Modal.Footer>
           <Button
             variant="primary"
-            hidden={!null && user.id === props.data.userId ? false : true}
+            hidden={null ? true: (user.id === props.data.userId ? false : true)}
             onClick={handleEdit}
           >
             Edit Post
